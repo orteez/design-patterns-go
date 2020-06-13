83dd65719316fc57739d0dfc6dd5c05a868d1be9
Christian Ortiz <chanorti@ucsc.edu>
2020-06-13 13:39:01 -0700

    Added syncroniztion to singleton

--- creational/singleton/singleton.go
+++ creational/singleton/singleton.go
@@ -1,28 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type logger struct {
 }
 
 type singleton struct {
 	instance *logger
+	lock     *sync.Mutex
 }
 
+var once sync.Once
+
 func (s *singleton) initialize() {
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	once.Do(func() {
+		fmt.Println("Intializing logger")
+		s.instance = &logger{}
+	})
+
 	if s.instance != nil {
 		fmt.Println("Logger already initialized.")
-		return
 	}
-
-	fmt.Println("Intializing logger")
-	s.instance = &logger{}
 }
 
 func main() {
-	obj := singleton{}
+	obj := singleton{lock: &sync.Mutex{}}
+	size := 5
+	var wg sync.WaitGroup
 
-	obj.initialize()
-	obj.initialize()
-	obj.initialize()
+	// you can also add these one at
+	// a time if you need to
+
+	wg.Add(size)
+
+	for i := 0; i < size; i++ {
+		go func(index int) {
+			defer wg.Done()
+			fmt.Printf("Trying to init. id:%d\n", index)
+			obj.initialize()
+		}(i)
+	}
+
+	wg.Wait()
 }
